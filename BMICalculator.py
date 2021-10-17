e1469ab7ea48652e85eef679b366c108124d1c58
Nishant Kumar <62173480+CosmicTechie@users.noreply.github.com>
2021-10-17 11:30:31 +0530

    Update BMICalculator.py

--- BMICalculator.py
+++ BMICalculator.py
@@ -3,12 +3,11 @@ Weight=float(input("Enter your Weight in Kg: "))
 Height = Height/100
 BMI=Weight/(Height*Height)
 print("Your Body Mass Index is: ",BMI)
-if(BMI>0):
-	if(BMI<=18.5):
-		print("Oops! You are underweight.")
-	elif(BMI<=24.9):
-		print("Awesome! You are healthy.")
-	elif(BMI<=29.9):
-		print("You are overweight.")
-	else: print("You are Obese. Start Exercise.")
-else:("Enter Valid Details")
+if(BMI<=18.5):
+	print("Oops! You are underweight.")
+elif(BMI<=24.9):
+	print("Awesome! You are healthy.")
+elif(BMI<=29.9):
+	print("You are overweight.")
+else:
+	print("You are Obese. Start Exercise.")
